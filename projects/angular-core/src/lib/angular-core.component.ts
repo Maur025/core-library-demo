30b97ac1ae923cfccbd8dd384322df5a59069331
Maur025 <mmoyapalli@gmail.com>
2024-11-20 20:03:40 -0400

    cambios iniciales, estruvtura y primer reutilizable

---
+++ projects/angular-core/src/lib/angular-core.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-core',
+  template: `
+    <p>
+      angular-core works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AngularCoreComponent {
+
+}
